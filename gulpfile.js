a6b48798464c60bde9c13583030f8eabefbbb895
toqi1 <Tony.Qian@ebuilder.com>
2016-05-31 10:40:20 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var webserver = require('gulp-webserver');
+var jsFiles = [
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/bootstrap/dist/js/bootstrap.min.js',
+    './bower_components/angular/angular.js'
+];
+var cssFiles = [
+    './bower_components/bootstrap/dist/css/bootstrap.min.css',
+    './bower_components/font-awesome/css/font-awesome.min.css',
+    './src/css/signin.css'
+];
+// 在这两个 `min` 任务之外，还有两个不带 `min` 的任务，区别在于不对文件压缩
+gulp.task('scripts_min', function(){
+    return gulp.src(jsFiles)
+        .pipe(concat('all.js')) // 合并 JavaScript ，并设置合并后的文件名
+        .pipe(uglify()) // 执行 JavaScript 压缩
+        .pipe(gulp.dest('./dist/js'));
+});
+gulp.task('stylesheets_min', function(){
+    return gulp.src(cssFiles)
+        .pipe(concat('all.css')) // 合并 CSS ，并设置合并后的文件名
+        .pipe(minifyCss()) // 执行 CSS 压缩
+        .pipe(gulp.dest('./dist/css'));
+});
+
+// copy files for build
+gulp.task('copyFiles', function () {
+    return gulp.src([
+        './src/assets/**/*',
+        './src/fonts/*',
+        './src/index.html'], {
+        base: './src/'
+    })
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', function(){
+    // 不同的文件个性，需要执行不同的任务来处理
+    gulp.watch(['bower_components/*'], ['scripts', 'stylesheets']);
+    gulp.watch(['src/css/*']);
+    gulp.watch(['app/controllers/*', 'app/modules/*', 'app/services/*'], ['scripts']);
+});
+
+gulp.task('webserver', function(){
+    gulp.src('./dist/')
+        .pipe(webserver({
+            livereload: true,
+            fallback: 'index.html'
+        }));
+});
+
+gulp.task('default',['scripts_min', 'stylesheets_min', 'copyFiles', 'webserver','watch']); //定义默认任务
