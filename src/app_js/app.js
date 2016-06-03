212447e89b77ca5522a46084ce144717fb10ae80
toqi1 <Tony.Qian@ebuilder.com>
2016-06-03 16:30:23 +0800

    add ng

---
+++ src/app_js/app.js
@@ -0,0 +1,63 @@
+angular
+    .module('turfApp', ['ui.router'])
+    .config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+          $urlRouterProvider.otherwise('/login');
+          $stateProvider
+              .state('index', {
+                  url: '/index',
+                  views: {
+                      '': {
+                          templateUrl: 'tpls/home.html'
+                      },
+                      'topbar@index': {
+                          templateUrl: 'tpls/topbar.html'
+                      },
+                      'sidebar@index': {
+                          templateUrl: 'tpls/sidebar.html'
+                      },
+                      'main@index': {
+                          templateUrl: 'tpls/tenantlist.html',
+                          controller: function($scope, $state) {
+                              $scope.addUserType = function() {
+                                  $state.go("index.addusertype");
+                              }
+                          }
+                      }
+                  }
+              })
+              .state('login', {
+                  url: '/login',
+                  views: {
+                      '': {
+                          templateUrl: 'tpls/login.html'
+                      }
+                  }
+              })
+              .state('index.addusertype', {
+                  url: '/addusertype',
+                  views: {
+                      'main@index': {
+                          templateUrl: 'tpls/addusertype.html'
+
+                      }
+                  }
+              })
+              .state('index.resetpw', {
+                  url: '/resetpw',
+                  views: {
+                      'main@index': {
+                          templateUrl: 'tpls/reset_password.html'
+
+                      }
+                  }
+              })
+              .state('index.change', {
+                  url: '/change',
+                  views: {
+                      'main@index': {
+                          templateUrl: 'tpls/change.html'
+
+                      }
+                  }
+              })
+      }]);
